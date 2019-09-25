cc671521757399a2bbb0ea11a631529786c73651
jhs <724033319@qq.com>
2019-09-25 09:10:33 +0800

    Spring day03 转账的demo 没有将转账这一动作变成一个事务

---
+++ day03_eesy_01account/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -0,0 +1,56 @@
+package com.itheima.service.impl;
+
+import com.itheima.dao.IAccountDao;
+import com.itheima.domain.Account;
+import com.itheima.service.IAccountService;
+
+import java.util.List;
+
+/**
+ * 账户的业务层实现类 不使用事务
+ */
+public class AccountServiceImpl implements IAccountService {
+
+    private IAccountDao accountDao;
+
+    public void setAccountDao(IAccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    @Override
+    public List<Account> findAllAccount() {
+        return accountDao.findAllAccount();
+    }
+
+    @Override
+    public Account findAccountById(Integer accountId) {
+        return accountDao.findAccountById(accountId);
+    }
+
+    @Override
+    public void saveAccount(Account account) {
+        accountDao.saveAccount(account);
+    }
+
+    @Override
+    public void updateAccount(Account account) {
+        accountDao.updateAccount(account);
+    }
+
+    @Override
+    public void deleteAccount(Integer acccountId) {
+        accountDao.deleteAccount(acccountId);
+    }
+
+    @Override
+    public void transfer(String sourceName, String targetName, Float money) {
+        Account accountA = accountDao.findAccountByName(sourceName);
+        Account accountB = accountDao.findAccountByName(targetName);
+        accountA.setMoney(accountA.getMoney() - money);
+        accountB.setMoney(accountB.getMoney() + money);
+        accountDao.updateAccount(accountA);
+        accountDao.updateAccount(accountB);
+        System.out.println("成功");
+
+    }
+}
