85e2f3e509412a2b80183b93b7cfd010bc83fde7
jhs <724033319@qq.com>
2019-09-28 15:09:15 +0800

    upddate 更新测试的

--- day03_eesy_01account/src/test/java/com/itheima/test/AccountServiceTest.java
+++ day03_eesy_01account/src/test/java/com/itheima/test/AccountServiceTest.java
@@ -4,6 +4,7 @@ import com.itheima.service.IAccountService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -11,10 +12,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * 使用Junit单元测试：测试我们的配置
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:bean.xml")
+@ContextConfiguration(locations = "classpath:beantransactionYouHua.xml")//beannotransaction beantransactionYouHua.xml,
 public class AccountServiceTest {
 
     @Autowired
+    @Qualifier("proxyAccountService")
     private  IAccountService as;
 
     @Test
